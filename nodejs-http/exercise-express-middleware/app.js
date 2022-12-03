9fe3d127fef4c23957a3326dde867c805083c509
c0a21135 <c0a211351b@edu.teu.ac.jp>
2022-12-03 22:44:59 +0900

    init_test

--- nodejs-http/exercise-express-middleware/app.js
+++ nodejs-http/exercise-express-middleware/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+function isAuthorized(req, res, next) {
+  const auth = req.headers.authorization;
+  if (auth === 'secretpassword') { //シークレットパスワード
+    next();
+  } else {
+    res.status(401);
+    res.send('Not permitted'); // パスワードが違うときの表示
+  }
+}
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/users", (req, res) => {
+app.get("/users", isAuthorized, (req, res) => {
   res.json([
     {
       id: 1,
--- nodejs-http/exercise-express-middleware/client.js
+++ nodejs-http/exercise-express-middleware/client.js
@@ -5,7 +5,9 @@ http.get(
     port: 3000,
     hostname: "localhost",
     path: "/users",
-    headers: {},
+    headers: {
+      authorization: 'secretpassword' // 接続するときのパスワードの設定？
+    },
   },
   (res) => {
     console.log("connected");
