6a86ab63698905fcdc684bec781dfeaf8f68f245
c0a21135 <c0a211351b@edu.teu.ac.jp>
2022-12-05 11:59:13 +0900

    productsの情報取得

--- nodejs-http/exercise-express-middleware/client.js
+++ nodejs-http/exercise-express-middleware/client.js
@@ -1,5 +1,6 @@
 const http = require("http");
 
+// URL「/users」の情報取得
 http.get(
   {
     port: 3000,
@@ -11,6 +12,29 @@ http.get(
   },
   (res) => {
     console.log("connected");
+    res.on("data", (chunk) => {
+      console.log("chunk", "" + chunk);
+    });
+    res.on("end", () => {
+      console.log("No more data");
+    });
+    res.on("close", () => {
+      console.log("Closing connection");
+    });
+  }
+);
+
+// URL「/products」の情報取得
+http.get(
+  {
+    port: 3000,
+    hostname: "localhost",
+    path: "/products",
+    headers: {},
+  },
+  (res) => {
+    console.log("connected");
+    console.log("get data /products")
     res.on("data", (chunk) => {
       console.log("chunk", "" + chunk);
     });
